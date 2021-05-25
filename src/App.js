5c367bcf290aa93bdf7fd2ae83e8e3331f6a3704
Miriam <miriam.martin@roiback.com>
2021-05-25 20:50:36 +0200

    fix: menu label displayed

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ export default class App extends Component {
         const categoryObj = this.props.categories.find(obj => obj['label'] === category);
 
         this.setState({
-            'selectedMenuItem': categoryObj.name,
+            'selectedMenuItem': categoryObj.label,
             'selectedMenuItemIcon': categoryIcon,
             'categorySelected': categoryObj,
             'displayedItems': categoryObj.items,
