8e4e8dbb5d41312273de606574dc07d616d9323d
Miriam <miriam.martin@roiback.com>
2021-05-26 22:20:12 +0200

    refactor: migrate error message styles from styled-components to SCSS

--- src/Views/ErrorMessage.jsx
+++ src/Views/ErrorMessage.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import styled, { css } from "styled-components";
 
 import {ReactComponent as NoResultsImage} from '../assets/images/no-results.svg';
 
-const StyledMessage = styled.div`
-    ${({ theme }) => css`
-        align-items: center;
-        color: ${theme.color.neutral000};
-        display: flex;
-        flex-direction: column;
-        padding-top: 50px;
-    `}
-`;
-
-const StyledImage = styled(NoResultsImage)`
-    ${({ theme }) => css`
-        fill: ${theme.color.neutral000};
-        height: auto;
-        width: 62px;
-        margin-bottom: 20px;
-    `}
-`;
-
 const ErrorMessage = ({ type }) => (
-    <StyledMessage>
-        <StyledImage />
-        <span>Whoops!</span>
-        <p>Sorry, we couldn\'t find any result matching</p>
-    </StyledMessage>
+    <section className="error-message">
+        <NoResultsImage className="error-message__image"/>
+        <span className="error-message__title">Whoops!</span>
+        <p className="error-message__description">Sorry, we couldn't find any result matching</p>
+    </section>
 );
 
 export default ErrorMessage;
