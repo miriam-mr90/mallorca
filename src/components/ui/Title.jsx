17dda72fc3d3fcaa7642ccf38412728ff32f2248
Miriam <miriam.martin@roiback.com>
2021-05-31 20:20:25 +0200

    refactor: migrate title styles from styled-components to scss

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -1,38 +1,10 @@
 import React from 'react';
-import styled, { css } from "styled-components";
-
-const StyledTitle = styled.h2`
-    ${({ theme }) => css`
-        align-items: center;
-        color: ${theme.color.neutral000};
-        display: flex;
-        justify-content: center;
-        text-transform: capitalize;
-        font-weight: 500;
-        font-size: 26px;
-        margin: 1.2rem auto 2rem;
-        max-width: max-content;
-
-        .icon {
-          font-size: 30px;
-          margin-right: 1rem;
-        }
-
-        @media ${theme.device.mobileL} {
-          font-size: 32px;
-
-          .icon {
-              font-size: 38px;
-          }
-        }
-    `}
-`;
 
 const Title = ({ title, icon }) => (
-    <StyledTitle>
+    <h2 className="title">
         { icon && <i className={`icon icon-${icon}`} />}
         {title}
-    </StyledTitle>
+    </h2>
 );
 
 export default Title;
